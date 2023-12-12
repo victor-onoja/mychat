c78a8565851d46a0eb6d49fc245c876066247c4e
victor-onoja <odohvictor47@gmail.com>
2023-12-12 19:05:04 +0100

    updated main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:mychat/features/auth/screens/user_information.dart';
 import 'package:mychat/firebase_options.dart';
 import 'package:mychat/router.dart';
 import 'package:mychat/widgets/colors.dart';
